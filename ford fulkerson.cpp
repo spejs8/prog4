90693e7f86be7ffb120a8263e79f89ee5709089c
spejs8 <spasev_andrej@yahoo.com>
2025-10-29 09:42:54 +0100

    e

--- ford fulkerson.cpp
+++ ford fulkerson.cpp
@@ -4,14 +4,22 @@
 
 using namespace std;
 
-int dfs (int i, int v, vector<vector<int> > &graf, vector<int> &poseteni , int minflow=10000000) {
+class pat {
+public:
+    int pocetno;
+    int krajno;
+    int tezina;
+};
+
+int dfs (int i, int v, vector<vector<int> > &graf, vector<int> &poseteni , vector<pat> &pateka , int minflow=10000000) {
     if (i==v-1) { return minflow; }
     poseteni[i]=1;
     for (int k=0; k<v; k++) {
         if (poseteni[k]==0 && graf[i][k]!=0) { 
-            int vrednost=dfs(k, v, graf, poseteni, min(minflow,graf[i][k]));
+            int vrednost=dfs(k, v, graf, poseteni,pateka, min(minflow,graf[i][k]));
             if (vrednost!=0) {
                 //cout<<i<<"->"<<k<<"("<<graf[i][k]<<")"<<",";
+                pateka.push_back({i,k,graf[i][k]});
                 graf[i][k]-=vrednost;
                 graf[k][i]+=vrednost;
                 return vrednost;
@@ -21,19 +29,8 @@ int dfs (int i, int v, vector<vector<int> > &graf, vector<int> &poseteni , int m
     return 0;
 }
 
-int ford_fulkerson (vector<vector<int> > graf) {
-    int flow=0;
-    while (1) {
-        vector<int> poseteni(graf.size(),0);
-        int p=dfs(0,graf.size(),graf,poseteni);
-        if (p==0) { break; }
-        flow+=p;
-    }
-    return flow;
-}
-
 int main () {
-    int e,v,minflow;
+    int e,v,flow=0;
     cin>>v>>e;
     vector<vector<int> > graf(v,vector<int>(v,0));
     for (int i=0; i<e; i++) {
@@ -41,16 +38,18 @@ int main () {
         cin>>tmp>>tmp1>>tmp2;
         graf[tmp][tmp1]=tmp2;
     }
-    minflow=ford_fulkerson(graf);
-    for (int i=0; i<v; i++) {
-        for (int j=0; j<v; j++) {
-            if (graf[i][j]!=0) {
-                int tmp=graf[i][j];
-                graf[i][j]=0;
-                minflow=min(minflow,ford_fulkerson(graf));
-                graf[i][j]=tmp;
-            }
+    while (1) {
+        vector<int> a(v,0);
+        vector<pat> b;
+        int p=dfs (0,v,graf,a,b);
+        if (p==0) { break; }
+        while (b.size()!=0) {
+            pat tmp=b.back();
+            b.pop_back();
+            cout<<tmp.pocetno<<"->"<<tmp.krajno<<"("<<tmp.tezina<<"), ";
         }
+        cout<<endl;
+        flow+=p;
     }
-    cout<<minflow;
+    cout<<flow;
 }
